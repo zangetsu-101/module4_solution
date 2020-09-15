0ae70244004573c20e09cdadf8d4dd0487dde8d7
zangetsu-101 <vaibhavthathola9012383847@gmail.com>
2020-09-15 15:06:19 +0530

    finalise

---
+++ SpeakGoodBye.js
@@ -0,0 +1,11 @@
+
+(function(window) { 
+	var byeSpeaker = {};
+	byeSpeaker.speakWord = "Good Bye";
+	byeSpeaker.speak = function (name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+  }
+    
+window.byeSpeaker = byeSpeaker;
+
+ })(window);
---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+
+(function(window) { 
+	var helloSpeaker = {};
+	helloSpeaker.speakWord = "Hello";
+	helloSpeaker.speak = function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+  }
+    
+window.helloSpeaker = helloSpeaker;
+
+ })(window);
